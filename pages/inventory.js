d269527459c824b284223a2013cf87d74e610882
Cyborn-NFT <general@cybornnft.com>
2022-03-29 22:17:06 +0300

    V1

--- pages/inventory.js
+++ pages/inventory.js
@@ -156,7 +156,7 @@ export default function Inventory() {
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                     <h3 className="text-3xl font-semibold">
-                      Modal Title
+                      Start Auction
                     </h3>
                     <button
                       className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
@@ -168,15 +168,28 @@ export default function Inventory() {
                     </button>
                   </div>
 
-                  <div className="relative p-6 flex-auto">
-                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                      I always felt like I could do anything. That’s the main
-                      thing people are controlled by! Thoughts- their perception
-                      of themselves! They're slowed down by their perception of
-                      themselves. If you're taught you can’t do anything, you
-                      won’t do anything. I was taught I could do everything.
-                    </p>
-                  </div>
+                  <form>
+                    <div className="mb-6">
+                      <label for="auction"  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Auction Start Price</label>
+                      <input type="text" id="text"  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Auction Start Price" required>
+                    </div>
+                    <div className="mb-6">
+                      <label for="text"  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Auction End Price</label>
+                      <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Auction End Price" required>
+                    </div>
+                    <div  className="flex items-start mb-6">
+                      <div  className="flex items-center h-5">
+                        <label for="auction"  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Start Auction Period</label>
+                        <input id="date" aria-describedby="remember" type="date"  className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+                      </div>
+                      <div  className="flex items-center h-5">
+                      <label for="auction"  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">End Auction Period</label>
+                        <input id="date" aria-describedby="remember" type="date"  className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+                      </div>
+
+                    </div>
+                    <button type="button"  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Start Auction</button>
+                  </form>
 
                   <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                     <button
