a98399f88e1455e69110fa2d09de66273953b415
Cyborn-NFT <general@cybornnft.com>
2022-03-29 23:03:17 +0300

    V1

--- pages/inventory.js
+++ pages/inventory.js
@@ -258,6 +258,7 @@ export default function Inventory() {
                       placeholder="Auction Asset Name"
                       className="mt-8 border rounded p-4 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                     />
+                    <br />
                     <button className="block w-full px-12 py-3 text-sm font-medium text-black rounded shadow bg-blue-400 sm:w-auto active:bg-lime-100 hover:bg-lime-300 focus:outline-none focus:ring">
                       Transfer
                     </button>
