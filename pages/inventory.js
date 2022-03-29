dab396bcf62f28fd12d9dd5d9b40b02d3f2581ac
Cyborn-NFT <general@cybornnft.com>
2022-03-29 22:38:12 +0300

    V1

--- pages/inventory.js
+++ pages/inventory.js
@@ -153,22 +153,22 @@ export default function Inventory() {
               className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             >
               <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-cybornheader outline-none focus:outline-none">
                   <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                    <h3 className="text-3xl font-semibold">
+                    <h3 className="text-3xl font-semibold text-white">
                       Start Auction
                     </h3>
                     <button
-                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      className="p-1 ml-auto bg-transparent border-0 text-white opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                       onClick={() => setShowModal(false)}
                     >
-                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      <span className="bg-black text-white opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                         ×
                       </span>
                     </button>
                   </div>
 
-                  <div className="relative p-36 flex-auto">
+                  <div className="relative p-6 flex-auto">
                     <input
                       placeholder="Auction Asset Name"
                       className="mt-8 border rounded p-4 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
