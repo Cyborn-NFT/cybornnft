7947154c4ff4e303b28191294253e7f0aa29bf4e
Cyborn-NFT <general@cybornnft.com>
2022-03-29 22:44:41 +0300

    V1

--- pages/inventory.js
+++ pages/inventory.js
@@ -168,7 +168,7 @@ export default function Inventory() {
                     </button>
                   </div>
 
-                  <div className="relative p-18 flex-auto">
+                  <div className="relative p-36 flex-auto">
                     <input
                       placeholder="Auction Asset Name"
                       className="mt-8 border rounded p-4 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
