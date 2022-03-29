59830f184156bed14afcddde8f90484f21b4c26f
Cyborn-NFT <general@cybornnft.com>
2022-03-29 22:32:25 +0300

    V1

--- pages/inventory.js
+++ pages/inventory.js
@@ -150,7 +150,7 @@ export default function Inventory() {
         {showModal ? (
           <>
             <div
-              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+              className="justify-center p-36 items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             >
               <div className="relative w-auto my-6 mx-auto max-w-3xl">
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
