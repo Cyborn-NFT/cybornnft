b61c7b341d94c31a57301eea9f9341e93561d5c0
Cyborn-NFT <general@cybornnft.com>
2022-03-29 22:53:52 +0300

    V1

--- pages/inventory.js
+++ pages/inventory.js
@@ -162,7 +162,7 @@ export default function Inventory() {
                       className="p-1 ml-auto bg-transparent border-0 text-white opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                       onClick={() => setShowModal(false)}
                     >
-                      <span className="bg-black text-white opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      <span className="bg-white text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
                         ×
                       </span>
                     </button>
