8de9ed2b3acef87d8cfc3681809e0e683b69523e
Cyborn-NFT <general@cybornnft.com>
2022-03-30 21:39:44 +0300

    V2

--- pages/polygon/signin.js
+++ pages/polygon/signin.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import styles from '../styles/Home.module.css'
+import styles from '/styles/Home.module.css'
 import Head from "next/head";
 import Link from "next/link";
 import { supabase } from '/client'
