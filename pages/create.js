ee62d4f3cd2499eaa132430470f6cae4450d0b4f
Cyborn-NFT <general@cybornnft.com>
2022-03-30 14:13:06 +0300

    v1.1

--- pages/create.js
+++ pages/create.js
@@ -139,6 +139,13 @@ function Create(){
 
               />
               <br />
+              <input
+                placeholder="Set Royalty (Max Limit 3%)"
+                className="mt-2 border rounded p-4 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                 min="0" max="3"
+
+              />
+              <br />
               <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select your protocol</label>
               <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option>Ethereum</option>
