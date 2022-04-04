ac48da06e2a4f90d88bebe8e02dbbf8aa3cff95e
Cyborn-NFT <general@cybornnft.com>
2022-04-04 15:46:15 +0300

    V2.1

--- pages/index.js
+++ pages/index.js
@@ -223,7 +223,7 @@ function Home() {
           <option value="Buy">Buy</option>
           <option value="Auction">Auction</option>
         </select>
-        <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <select onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           <option value="">Search Protocol</option>
           <option value="/">ETH</option>
           <option value="/polygon">Matic</option>
