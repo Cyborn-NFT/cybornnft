e9d2bd391323fc620a54ee35604ac0c97f9164fa
surenravi1407 <surenravi1407@gmail.com>
2022-07-05 10:29:51 +0300

    docker

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
+    reactStrictMode: true,
+    eslint: {
         ignoreDuringBuilds: true,
     },
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    webpackDevMiddleware: (config) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+        return config;
+    },
+};
